d2134505d34748d3921edc8490aec62c1d70bb14
clooooode <jackey8616@gmail.com>
2019-06-04 13:22:32 +0800

    Fix FetcherHjwzw's fetch method

--- silverfish/usecase/fetcher_hjwzw.go
+++ silverfish/usecase/fetcher_hjwzw.go
@@ -48,15 +48,16 @@ func (fh *FetcherHjwzw) FetchNovelInfo(url *string) *entity.Novel {
 	doc := fh.FetchDoc(url)
 
 	id := fh.GenerateID(url)
-	infoAnchor := doc.Find("#form1 > div > table > tbody > tr > td > h1").Parent().Parent().Parent()
-	title := infoAnchor.Find("tr > td > h1").Text()
-	author := infoAnchor.Find("tr:nth-child(2) > td:nth-child(1) > a:nth-child(1)").Text()
-	description, ok := doc.Find("meta[name='Description']").Attr("content")
-	coverURL := strings.Replace(*url, "Book/Chapter", "images/id", 1) + ".jpg"
-	if title == "" || author == "" || !ok || coverURL == "" {
+	title, ok0 := doc.Find("meta[property='og:novel:book_name']").Attr("content")
+	author, ok1 := doc.Find("meta[property='og:novel:author']").Attr("content")
+	description, ok2 := doc.Find("meta[property='og:description']").Attr("content")
+	coverURL, ok3 := doc.Find("meta[property='og:image']").Attr("content")
+	if !ok0 || !ok1 || !ok2 || !ok3 {
 		log.Printf("Something missing, title: %s, author: %s, description: %s, coverURL: %s", title, author, description, coverURL)
 		return nil
 	}
+	chapterURL := strings.Replace(*url, "Book", "Book/Chapter", 1)
+	doc = fh.FetchDoc(&chapterURL)
 
 	chapters := []entity.NovelChapter{}
 	doc.Find("div#tbchapterlist > table > tbody > tr > td > a").Each(func(i int, s *goquery.Selection) {
