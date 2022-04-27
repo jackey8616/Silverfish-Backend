c70db54cdccbc342202513888861a454e48a1030
Clooooode <jackey8616@gmail.com>
2022-04-27 22:29:58 +0800

    fix: Novel & Comic route duplication

--- router/api/v1/comic.go
+++ router/api/v1/comic.go
@@ -13,24 +13,23 @@ import (
 
 // BlueprintComicv1 export
 type BlueprintComicv1 struct {
-	auth  *silverfish.Auth
-	user  *silverfish.User
-	comic *silverfish.Comic
-	route string
+	authSer  *silverfish.Auth
+	userSer  *silverfish.User
+	comicSer *silverfish.Comic
+	route    string
 }
 
 // NewBlueprintComicv1 export
 func NewBlueprintComicv1(
-	auth *silverfish.Auth,
-	user *silverfish.User,
-	comic *silverfish.Comic,
+	authSer *silverfish.Auth,
+	userSer *silverfish.User,
+	comicSer *silverfish.Comic,
 ) *BlueprintComicv1 {
 	bpc := new(BlueprintComicv1)
-	bpc.auth = auth
-	bpc.user = user
-	bpc.comic = comic
-	bpc.auth = auth
-	bpc.route = "/comic"
+	bpc.authSer = authSer
+	bpc.userSer = userSer
+	bpc.comicSer = comicSer
+	bpc.route = "/comics"
 	return bpc
 }
 
@@ -39,59 +38,51 @@ func (bpc *BlueprintComicv1) RouteRegister(parentRouter *mux.Router) {
 	router := parentRouter.PathPrefix(bpc.route).Subrouter()
 	router.HandleFunc("", bpc.root).Methods("GET", "POST")
 	router.HandleFunc("/", bpc.root).Methods("GET", "POST")
-	router.HandleFunc("/{comicID}", bpc.deleteComic).Methods("DELETE")
-	router.HandleFunc("/chapter", bpc.chapter).Methods("GET")
-
-	sRouter := parentRouter.PathPrefix(bpc.route + "s").Subrouter()
-	sRouter.HandleFunc("", bpc.listComic).Methods("GET")
-	sRouter.HandleFunc("/", bpc.listComic).Methods("GET")
-	sRouter.HandleFunc("/{comicID}", bpc.deleteComic).Methods("DELETE")
-	sRouter.HandleFunc("/chapter", bpc.chapter).Methods("GET")
+	router.HandleFunc("/{comicID}", bpc.comic).Methods("GET", "DELETE")
+	router.HandleFunc("/{comicID}/chapter/{chapterIndex}", bpc.chapter).Methods("GET")
 }
 
 func (bpc *BlueprintComicv1) root(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 	isAdmin := false
 	sessionToken := r.Header.Get("Authorization")
 	if sessionToken != "" {
-		session, err := bpc.auth.GetSession(&sessionToken)
+		session, err := bpc.authSer.GetSession(&sessionToken)
 		if err != nil {
 			isAdmin = false
-		} else if accountIsAdmin, _ := bpc.auth.IsAdmin(session.GetAccount()); accountIsAdmin == true {
+		} else if accountIsAdmin, _ := bpc.authSer.IsAdmin(session.GetAccount()); accountIsAdmin == true {
 			isAdmin = true
 		}
 	}
 
 	switch r.Method {
 	case http.MethodGet:
-		comicID := r.URL.Query().Get("comic_id")
+		comicID := params["comicID"]
 		if comicID != "" {
-			w.Header().Set("Content-Type", "application/json")
-
-			response := new(entity.APIResponse)
-			result, err := bpc.comic.GetComicByID(&comicID)
+			result, err := bpc.comicSer.GetComicByID(&comicID)
 			if (err != nil && err.Error() == "not found") ||
 				(result != nil && !result.IsEnable && !isAdmin) {
 				w.WriteHeader(http.StatusNotFound)
 			} else {
-				response = entity.NewAPIResponse(result, err)
+				response := entity.NewAPIResponse(result, err)
+				js, _ := json.Marshal(response)
+				w.Write(js)
 			}
+		} else {
+			result, err := bpc.comicSer.GetComics(isAdmin)
+			response := entity.NewAPIResponse(result, err)
 			js, _ := json.Marshal(response)
 			w.Write(js)
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
 		}
 	case http.MethodPost:
-		w.Header().Set("Content-Type", "application/json")
-
 		response := new(entity.APIResponse)
 		if !isAdmin {
 			response = entity.NewAPIResponse(nil, errors.New("Only Admin allowed"))
 		} else {
 			comicURL := r.FormValue("comic_url")
 			if comicURL != "" {
-				w.Header().Set("Content-Type", "application/json")
-
-				result, err := bpc.comic.AddComicByURL(&comicURL)
+				result, err := bpc.comicSer.AddComicByURL(&comicURL)
 				response = entity.NewAPIResponse(result, err)
 			} else {
 				response = entity.NewAPIResponse(nil, errors.New("Field comic_url should not be empty"))
@@ -104,53 +95,46 @@ func (bpc *BlueprintComicv1) root(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (bpc *BlueprintComicv1) deleteComic(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodDelete:
-		sessionToken := r.Header.Get("Authorization")
-		w.Header().Set("Content-Type", "application/json")
-
-		session, err := bpc.auth.GetSession(&sessionToken)
-		response := new(entity.APIResponse)
-		if err != nil {
-			response = entity.NewAPIResponse(nil, err)
-		} else if isAdmin, _ := bpc.auth.IsAdmin(session.GetAccount()); isAdmin == false {
-			response = entity.NewAPIResponse(nil, errors.New("Only Admin allowed"))
-		} else {
-			params := mux.Vars(r)
-			comicID := params["comicID"]
-
-			if comicID != "" {
-				err := bpc.comic.RemoveComicByID(&comicID)
-				response = entity.NewAPIResponse(nil, err)
-			} else {
-				response = entity.NewAPIResponse(nil, errors.New("Field comic_id should not be empty"))
-			}
-		}
-		js, _ := json.Marshal(response)
-		w.Write(js)
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+func (bpc *BlueprintComicv1) comic(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	comicID := params["comicID"]
+	if comicID == "" {
+		w.WriteHeader(http.StatusBadRequest)
 	}
-}
 
-func (bpc *BlueprintComicv1) listComic(w http.ResponseWriter, r *http.Request) {
+	isAdmin := false
+	sessionToken := r.Header.Get("Authorization")
+	if sessionToken != "" {
+		session, err := bpc.authSer.GetSession(&sessionToken)
+		if err != nil {
+			isAdmin = false
+		} else if accountIsAdmin, _ := bpc.authSer.IsAdmin(session.GetAccount()); accountIsAdmin == true {
+			isAdmin = true
+		}
+	}
+
 	switch r.Method {
 	case http.MethodGet:
-		w.Header().Set("Content-Type", "application/json")
-		shouldFetchDisable := false
-		sessionToken := r.Header.Get("Authorization")
-		if sessionToken != "" {
-			session, _ := bpc.auth.GetSession(&sessionToken)
-			if session == nil {
-				shouldFetchDisable = false
-			} else if isAdmin, _ := bpc.auth.IsAdmin(session.GetAccount()); isAdmin == true {
-				shouldFetchDisable = true
-			}
+		result, err := bpc.comicSer.GetComicByID(&comicID)
+		if (err != nil && err.Error() == "not found") ||
+			(result != nil && !result.IsEnable && !isAdmin) {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			response := entity.NewAPIResponse(result, err)
+			js, _ := json.Marshal(response)
+			w.Write(js)
 		}
-
-		result, err := bpc.comic.GetComics(shouldFetchDisable)
-		response := entity.NewAPIResponse(result, err)
+	case http.MethodDelete:
+		response := new(entity.APIResponse)
+		if isAdmin == false {
+			response = entity.NewAPIResponse(nil, errors.New("Only Admin allowed"))
+		} else if comicID != "" {
+			err := bpc.comicSer.RemoveComicByID(&comicID)
+			response = entity.NewAPIResponse(nil, err)
+		} else {
+			response = entity.NewAPIResponse(nil, errors.New("Field comicID should not be empty"))
+		}
 		js, _ := json.Marshal(response)
 		w.Write(js)
 	default:
@@ -159,25 +143,25 @@ func (bpc *BlueprintComicv1) listComic(w http.ResponseWriter, r *http.Request) {
 }
 
 func (bpc *BlueprintComicv1) chapter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	comicID := params["comicID"]
+	chapterIndex := params["chapterIndex"]
+	if comicID == "" || chapterIndex == "" {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	sessionToken := r.Header.Get("Authorization")
+	session, _ := bpc.authSer.GetSession(&sessionToken)
+
 	switch r.Method {
 	case http.MethodGet:
-		sessionToken := r.Header.Get("Authorization")
-		session, _ := bpc.auth.GetSession(&sessionToken)
-
-		comicID := r.URL.Query().Get("comic_id")
-		chapterIndex := r.URL.Query().Get("chapter_index")
-		if comicID == "" || chapterIndex == "" {
-			w.WriteHeader(http.StatusBadRequest)
-		} else {
-			w.Header().Set("Content-Type", "application/json")
-			result, err := bpc.comic.GetComicChapter(&comicID, &chapterIndex)
-			response := entity.NewAPIResponse(result, err)
-			if err == nil && session != nil {
-				go bpc.user.UpdateBookmark("Comic", &comicID, session.GetAccount(), &chapterIndex)
-			}
-			js, _ := json.Marshal(response)
-			w.Write(js)
+		result, err := bpc.comicSer.GetComicChapter(&comicID, &chapterIndex)
+		response := entity.NewAPIResponse(result, err)
+		if err == nil && session != nil {
+			go bpc.userSer.UpdateBookmark("Comic", &comicID, session.GetAccount(), &chapterIndex)
 		}
+		js, _ := json.Marshal(response)
+		w.Write(js)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
--- router/api/v1/novel.go
+++ router/api/v1/novel.go
@@ -13,23 +13,23 @@ import (
 
 // BlueprintNovelv1 export
 type BlueprintNovelv1 struct {
-	auth  *silverfish.Auth
-	user  *silverfish.User
-	novel *silverfish.Novel
-	route string
+	authSer  *silverfish.Auth
+	userSer  *silverfish.User
+	novelSer *silverfish.Novel
+	route    string
 }
 
 // NewBlueprintNovelv1 export
 func NewBlueprintNovelv1(
-	auth *silverfish.Auth,
-	user *silverfish.User,
-	novel *silverfish.Novel,
+	authSer *silverfish.Auth,
+	userSer *silverfish.User,
+	novelSer *silverfish.Novel,
 ) *BlueprintNovelv1 {
 	bpn := new(BlueprintNovelv1)
-	bpn.auth = auth
-	bpn.user = user
-	bpn.novel = novel
-	bpn.route = "/novel"
+	bpn.authSer = authSer
+	bpn.userSer = userSer
+	bpn.novelSer = novelSer
+	bpn.route = "/novels"
 	return bpn
 }
 
@@ -38,62 +38,51 @@ func (bpn *BlueprintNovelv1) RouteRegister(parentRouter *mux.Router) {
 	router := parentRouter.PathPrefix(bpn.route).Subrouter()
 	router.HandleFunc("", bpn.root).Methods("GET", "POST")
 	router.HandleFunc("/", bpn.root).Methods("GET", "POST")
-	router.HandleFunc("/{novelID}", bpn.deleteNovel).Methods("DELETE")
-	/* TODO: route should be /api/v1/novel/chapter
-	This change will need to update Frontend's api calling. */
-	parentRouter.HandleFunc("/chapter", bpn.chapter).Methods("GET")
-	router.HandleFunc("/chapter", bpn.chapter).Methods("GET")
-
-	sRouter := parentRouter.PathPrefix(bpn.route + "s").Subrouter()
-	sRouter.HandleFunc("", bpn.listNovel).Methods("GET")
-	sRouter.HandleFunc("/", bpn.listNovel).Methods("GET")
-	sRouter.HandleFunc("/{novelID}", bpn.deleteNovel).Methods("DELETE")
-	sRouter.HandleFunc("/chapter", bpn.chapter).Methods("GET")
+	router.HandleFunc("/{novelID}", bpn.novel).Methods("GET", "DELETE")
+	router.HandleFunc("/{novelID}/chapter/{chapterIndex}", bpn.chapter).Methods("GET")
 }
 
 func (bpn *BlueprintNovelv1) root(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 	isAdmin := false
 	sessionToken := r.Header.Get("Authorization")
 	if sessionToken != "" {
-		session, err := bpn.auth.GetSession(&sessionToken)
+		session, err := bpn.authSer.GetSession(&sessionToken)
 		if err != nil {
 			isAdmin = false
-		} else if accountIsAdmin, _ := bpn.auth.IsAdmin(session.GetAccount()); accountIsAdmin == true {
+		} else if accountIsAdmin, _ := bpn.authSer.IsAdmin(session.GetAccount()); accountIsAdmin == true {
 			isAdmin = true
 		}
 	}
 
 	switch r.Method {
 	case http.MethodGet:
-		novelID := r.URL.Query().Get("novel_id")
+		novelID := params["novelID"]
 		if novelID != "" {
-			w.Header().Set("Content-Type", "application/json")
-
-			response := new(entity.APIResponse)
-			result, err := bpn.novel.GetNovelByID(&novelID)
+			result, err := bpn.novelSer.GetNovelByID(&novelID)
 			if (err != nil && err.Error() == "not found") ||
 				(result != nil && !result.IsEnable && !isAdmin) {
 				w.WriteHeader(http.StatusNotFound)
 			} else {
-				response = entity.NewAPIResponse(result, err)
+				response := entity.NewAPIResponse(result, err)
+				js, _ := json.Marshal(response)
+				w.Write(js)
 			}
+		} else {
+			result, err := bpn.novelSer.GetNovels(isAdmin)
+			response := entity.NewAPIResponse(result, err)
 			js, _ := json.Marshal(response)
 			w.Write(js)
-		} else {
-			w.WriteHeader(http.StatusBadRequest)
 		}
 	case http.MethodPost:
-		w.Header().Set("Content-Type", "application/json")
-
 		response := new(entity.APIResponse)
 		if !isAdmin {
 			response = entity.NewAPIResponse(nil, errors.New("Only Admin allowed"))
 		} else {
 			novelURL := r.FormValue("novel_url")
 			if novelURL != "" {
-				w.Header().Set("Content-Type", "application/json")
-
-				result, err := bpn.novel.AddNovelByURL(&novelURL)
+				result, err := bpn.novelSer.AddNovelByURL(&novelURL)
 				response = entity.NewAPIResponse(result, err)
 			} else {
 				response = entity.NewAPIResponse(nil, errors.New("Field novel_url should not be empty"))
@@ -106,53 +95,46 @@ func (bpn *BlueprintNovelv1) root(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (bpn *BlueprintNovelv1) deleteNovel(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodDelete:
-		sessionToken := r.Header.Get("Authorization")
-		w.Header().Set("Content-Type", "application/json")
-
-		session, err := bpn.auth.GetSession(&sessionToken)
-		response := new(entity.APIResponse)
-		if err != nil {
-			response = entity.NewAPIResponse(nil, err)
-		} else if isAdmin, _ := bpn.auth.IsAdmin(session.GetAccount()); isAdmin == false {
-			response = entity.NewAPIResponse(nil, errors.New("Only Admin allowed"))
-		} else {
-			params := mux.Vars(r)
-			novelID := params["novelID"]
-
-			if novelID != "" {
-				err := bpn.novel.RemoveNovelByID(&novelID)
-				response = entity.NewAPIResponse(nil, err)
-			} else {
-				response = entity.NewAPIResponse(nil, errors.New("Field novel_id should not be empty"))
-			}
-		}
-		js, _ := json.Marshal(response)
-		w.Write(js)
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+func (bpn *BlueprintNovelv1) novel(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	novelID := params["novelID"]
+	if novelID == "" {
+		w.WriteHeader(http.StatusBadRequest)
 	}
-}
 
-func (bpn *BlueprintNovelv1) listNovel(w http.ResponseWriter, r *http.Request) {
+	isAdmin := false
+	sessionToken := r.Header.Get("Authorization")
+	if sessionToken != "" {
+		session, err := bpn.authSer.GetSession(&sessionToken)
+		if err != nil {
+			isAdmin = false
+		} else if accountIsAdmin, _ := bpn.authSer.IsAdmin(session.GetAccount()); accountIsAdmin == true {
+			isAdmin = true
+		}
+	}
+
 	switch r.Method {
 	case http.MethodGet:
-		w.Header().Set("Content-Type", "application/json")
-		shouldFetchDisable := false
-		sessionToken := r.Header.Get("Authorization")
-		if sessionToken != "" {
-			session, _ := bpn.auth.GetSession(&sessionToken)
-			if session == nil {
-				shouldFetchDisable = false
-			} else if isAdmin, _ := bpn.auth.IsAdmin(session.GetAccount()); isAdmin == true {
-				shouldFetchDisable = true
-			}
+		result, err := bpn.novelSer.GetNovelByID(&novelID)
+		if (err != nil && err.Error() == "not found") ||
+			(result != nil && !result.IsEnable && !isAdmin) {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			response := entity.NewAPIResponse(result, err)
+			js, _ := json.Marshal(response)
+			w.Write(js)
 		}
-
-		result, err := bpn.novel.GetNovels(shouldFetchDisable)
-		response := entity.NewAPIResponse(result, err)
+	case http.MethodDelete:
+		response := new(entity.APIResponse)
+		if isAdmin == false {
+			response = entity.NewAPIResponse(nil, errors.New("Only Admin allowed"))
+		} else if novelID != "" {
+			err := bpn.novelSer.RemoveNovelByID(&novelID)
+			response = entity.NewAPIResponse(nil, err)
+		} else {
+			response = entity.NewAPIResponse(nil, errors.New("Field novelID should not be empty"))
+		}
 		js, _ := json.Marshal(response)
 		w.Write(js)
 	default:
@@ -161,26 +143,25 @@ func (bpn *BlueprintNovelv1) listNovel(w http.ResponseWriter, r *http.Request) {
 }
 
 func (bpn *BlueprintNovelv1) chapter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	novelID := params["novelID"]
+	chapterIndex := params["chapterIndex"]
+	if novelID == "" || chapterIndex == "" {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	sessionToken := r.Header.Get("Authorization")
+	session, _ := bpn.authSer.GetSession(&sessionToken)
+
 	switch r.Method {
 	case http.MethodGet:
-		sessionToken := r.Header.Get("Authorization")
-		session, _ := bpn.auth.GetSession(&sessionToken)
-
-		novelID := r.URL.Query().Get("novel_id")
-		chapterIndex := r.URL.Query().Get("chapter_index")
-		if novelID == "" || chapterIndex == "" {
-			w.WriteHeader(http.StatusBadRequest)
-		} else {
-			w.Header().Set("Content-Type", "application/json")
-
-			result, err := bpn.novel.GetNovelChapter(&novelID, &chapterIndex)
-			response := entity.NewAPIResponse(result, err)
-			if err == nil && session != nil {
-				go bpn.user.UpdateBookmark("Novel", &novelID, session.GetAccount(), &chapterIndex)
-			}
-			js, _ := json.Marshal(response)
-			w.Write(js)
+		result, err := bpn.novelSer.GetNovelChapter(&novelID, &chapterIndex)
+		response := entity.NewAPIResponse(result, err)
+		if err == nil && session != nil {
+			go bpn.userSer.UpdateBookmark("Novel", &novelID, session.GetAccount(), &chapterIndex)
 		}
+		js, _ := json.Marshal(response)
+		w.Write(js)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
