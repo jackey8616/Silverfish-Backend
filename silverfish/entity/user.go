ba9f4d7941b35da14a99f7ebc390953290cf7a12
Clooooode <12930377+jackey8616@users.noreply.github.com>
2025-02-01 21:19:03 +0800

    fix(User): incorrect partition key (#3)

--- silverfish/entity/user.go
+++ silverfish/entity/user.go
@@ -4,11 +4,11 @@ import "time"
 
 // User export
 type User struct {
-	Id                string    `json:"id" bson:"id"`
-	IsAdmin           bool      `json:"isAdmin" bson:"isAdmin"`
-	Account           string    `json:"account" bson:"account"`
-	Password          string    `json:"password" bson:"password"`
-	RegisterDatetime  time.Time `json:"registerDatetime" bson:"registerDatetime"`
-	LastLoginDatetime time.Time `json:"lastLoginDatetime" bson:"lastLoginDatetime"`
-	Bookmark          *Bookmark `json:"bookmark" bson:"bookmark"`
+	Id                string
+	IsAdmin           bool
+	Account           string // Partition Key
+	Password          string
+	RegisterDatetime  time.Time
+	LastLoginDatetime time.Time
+	Bookmark          *Bookmark
 }
